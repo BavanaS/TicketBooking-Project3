67a0cc95303f87d842d4b8af56b8accce4e465b3
Bavana <112500726+BavanaS@users.noreply.github.com>
2023-12-27 10:42:53 +0530

    Update City.java

--- IntegrationTesting/src/test/java/org/example/City.java
+++ IntegrationTesting/src/test/java/org/example/City.java
@@ -2,11 +2,13 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class City
 {
-    public void enterCity(WebDriver driver,String city)
+    public void enterCity(WebDriver driver,String city) throws InterruptedException
     {
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@placeholder='Search for your city']")).click();
         driver.findElement(By.xpath("//input[@placeholder='Search for your city']")).sendKeys(city);
 
